d23dc26fc9fc62e2f6607599c3f26730f70de983
TatevikNavasardyan <tatev0708@gmail.com>
2024-10-01 23:19:50 +0400

    homework 4

---
+++ src/main/java/org/example/Homework_4/n_to_k.java
@@ -0,0 +1,23 @@
+package org.example.Homework_4;
+
+import java.util.Scanner;
+
+public class n_to_k {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+        System.out.println("Enter another one");
+        int k = sc.nextInt();
+        System.out.println(nk(n, k));
+    }
+
+    public static int nk(int n, int k) {
+        if (n == 1 || k == 1) {
+            return n;
+        } else {
+            return n * nk(n, k-1);
+        }
+
+    }
+}
